d4ea820c3022e4464356510d9d04883affa834eb
XiaoyuGuo <16126144@bjtu.edu.cn>
2017-03-27 00:09:51 +0800

    123

---
+++ start_project/library/imports/7b/7bd40a47-87d7-444d-9989-2a212f8dec1d.js
@@ -0,0 +1,100 @@
+cc.Class({
+    "extends": cc.Component,
+
+    properties: {
+        jumpHeight: 0,
+        jumpDuration: 0,
+        maxMoveSpeed: 0,
+        accel: 0
+    },
+
+    // foo: {
+    //    default: null,      // The default value will be used only when the component attaching
+    //                           to a node for the first time
+    //    url: cc.Texture2D,  // optional, default is typeof default
+    //    serializable: true, // optional, default is true
+    //    visible: true,      // optional, default is true
+    //    displayName: 'Foo', // optional
+    //    readonly: false,    // optional, default is false
+    // },
+    // ...
+    // use this for initialization
+    onLoad: function onLoad() {
+        this.jumpAction = this.setJumpAction();
+
+        // 加速度方向开关
+        this.accLeft = false;
+        this.accRight = false;
+        // 主角当前水平方向速度
+        this.xSpeed = 50;
+
+        this.isJump = false;
+        // 初始化键盘输入监听
+        this.setInputControl();
+    },
+
+    setJumpAction: function setJumpAction() {
+
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        var finishJump = cc.callFunc(function () {
+            this.isJump = false;
+        }, this);
+        return cc.sequence(jumpUp, jumpDown, finishJump);
+    },
+
+    setInputControl: function setInputControl() {
+        var self = this;
+        // 添加键盘事件监听
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            // 有按键按下时，判断是否是我们指定的方向控制键，并设置向对应方向加速
+            onKeyPressed: function onKeyPressed(keyCode, event) {
+                switch (keyCode) {
+                    case cc.KEY.a:
+                        self.accLeft = true;
+                        self.accRight = false;
+                        break;
+                    case cc.KEY.d:
+                        self.accLeft = false;
+                        self.accRight = true;
+                        break;
+                    case cc.KEY.w:
+                        if (!self.isJump) {
+                            self.isJump = true;
+                            self.node.runAction(self.jumpAction);
+                        }
+
+                        break;
+                }
+            },
+            // 松开按键时，停止向该方向的加速
+            onKeyReleased: function onKeyReleased(keyCode, event) {
+                switch (keyCode) {
+                    case cc.KEY.a:
+                        self.accLeft = false;
+                        break;
+                    case cc.KEY.d:
+                        self.accRight = false;
+                        break;
+                }
+            }
+        }, self.node);
+    },
+
+    update: function update(dt) {
+        // 根据当前加速度方向每帧更新速度
+        if (this.accLeft) {
+            this.xSpeed = -500;
+            this.node.x += this.xSpeed * dt;
+        } else if (this.accRight) {
+            this.xSpeed = 500;
+            this.node.x += this.xSpeed * dt;
+        }
+        // 根据当前速度更新主角的位置
+    }
+});
+// called every frame, uncomment this function to activate update callback
+// update: function (dt) {
+
+// },
